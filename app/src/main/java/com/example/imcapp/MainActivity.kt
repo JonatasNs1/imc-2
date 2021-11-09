3aaf71aedf859fc2ccac1d93e33b92878a5569d6
Jonatas <jonatassantos152409@gmail.com>
2021-11-09 16:54:57 -0300

    aula 09/11

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.example.imcapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.cadastro)
+        setContentView(R.layout.activity_main)
 
-//        supportActionBar!!.hide()
+        // remover a toolbar
+      supportActionBar!!.hide()
+
+        //instanciar o botao entrar, findViewById<>() - encontrar uma view pelo o id
+        val botaoCriarConta = findViewById<Button>(R.id.buton_criar_conta)
+
+        //criar um listener - ouvinte, android fica de olho nesse botao (click)
+        botaoCriarConta.setOnClickListener {
+            //criar uma intent(), intent precisa de dois parametros, a intent v activiny que está tendo a intent
+            val abrirCadastro = Intent(this, Cadastro::class.java)
+            startActivity(abrirCadastro)
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_cadastro,menu)
-        return true
-    }
+
 }
