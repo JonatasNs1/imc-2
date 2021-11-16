f4fd782af2449e0977ead83621007acc7382ad08
Jonatas <jonatassantos152409@gmail.com>
2021-11-16 14:49:29 -0300

    16/11

--- app/src/main/java/com/example/imcapp/Cadastro.kt
+++ app/src/main/java/com/example/imcapp/Cadastro.kt
@@ -3,12 +3,19 @@ package com.example.imcapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
 
 class Cadastro : AppCompatActivity() {
+
+    //criando objeto, declarando ele
+    lateinit var editEmail:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cadastro)
 
+        editEmail = findViewById(R.id.edit_novo_email)
 //        supportActionBar!!.hide()
     }
 
@@ -16,4 +23,21 @@ class Cadastro : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_cadastro,menu)
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Salvar os dados do usuário em um sharedPreferences
+        //Criar um arquivo sharedPreferences
+        // Se o arquivo já existir o arquivo será aberto
+        val dados = getSharedPreferences("cadastro", MODE_PRIVATE)
+
+        //Criar um editor para o arquivo
+        //vou comecar a editar o arquivo dados
+        val editor = dados.edit()
+        //put - colocar
+        editor.putString("email", editEmail.text.toString())
+
+
+        return true
+    }
+
 }
